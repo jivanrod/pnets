e1621dcbed1000e4cc3b577462f8222ab220ca27
Sebastien Mamessier <sebastien.mamessier@gatech.edu>
2016-03-05 21:55:57 -0500

    Added ROS extension for places

---
+++ src/extensions/places/place-ros.ts
@@ -0,0 +1,47 @@
+import {Place,Token} from '../../petri';
+import _ = require('lodash');
+import {Topic,Ros,Message} from 'ROSLIB';
+
+/* Don't know if useful yet
+export interface RosFloat32 {
+  data: number
+}
+export interface RosInt32 {
+  data: number
+}
+*/
+
+
+/**
+* Place getting tokens from a ROS topic
+*/
+export class RosPlace<T> extends Place{
+
+  private topicHandle: any;
+
+  /**
+  * RosPlace constructor
+  * @param name Name of the petri place
+  * @param topic Ros topic to subscribe to. (Must be Float32 right now)
+  * @param throttle Period at which topic message is sent through Roslibjs
+  * @param tokenFn Callback mapping a Ros msg to a token array
+  */
+	constructor(name: string, topic: string, type: string,
+              private tokenFn: (Message) => Token[], throttle: number, rosHandle: Ros){
+    // Calling super class Place constructor
+    super(name);
+    // Creating topic
+    this.topicHandle = new Topic({
+      ros: rosHandle,
+      name: topic,
+      messageType: type,
+      throttle_rate: throttle
+    })
+    // Implementing ROS callback
+    this.topicHandle.subscribe( (msg) => {
+      this.setTokens(tokenFn(msg));
+    })
+	}
+
+
+}
